da06c6739ed4d164bd97c6eb4816393d134c915c
Rishi Pomal <38519496+rishi07it417@users.noreply.github.com>
2020-04-04 16:52:26 +0530

    Add files via upload

---
+++ TestProject/src/com/test/Student.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Student {
+	
+	int age;
+	int std;
+	String name;
+	
+	Student(int a,int s, String nme){
+		this.age = a;
+		this.std = s;
+		this.name = nme;
+	}
+
+}
